301192072f281589897ba35d4ac702895b274d03
Piyu0423 <piumikamadubashini5@gmail.com>
2020-12-01 21:15:03 +0530

    set up cookie

--- app.js
+++ app.js
@@ -28,47 +28,63 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-function auth (req,res,next){
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('You are not authenticated !');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
+app.use(cookieParser('12345-67890-09876-54321'));
+
+function auth(req, res, next) {
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      var err = new Error('You are not authenticated !');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+  //set cookie
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user','admin',{signed:true})
+      next();
+    }
+
+    else {
+      var err = new Error('You are not authenticated !');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
-
-  var auth = new Buffer (authHeader.split(' ')[1],'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-if (username === 'admin' && password === 'password'){
-  next();
-}
-else{
-  var err = new Error('You are not authenticated !');
-  res.setHeader('WWW-Authenticate','Basic');
-  err.status = 401;
-  return next(err);
-}
-
-}
+  else{
+    if(req.signedCookies.user==='admin'){
+    next();
+    }
+    else{
+      var err = new Error('You are not authenticated !');
+      err.status = 401;
+      return next(err);
+    }
+  }
+  }
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
