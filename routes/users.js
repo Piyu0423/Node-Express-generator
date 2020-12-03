92ace539e60a825ef297aab5441f76506df1405c
Piyu0423 <piumikamadubashini5@gmail.com>
2020-12-03 22:45:37 +0530

    mongoose population-author reference

--- routes/users.js
+++ routes/users.js
@@ -12,21 +12,33 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({ username: req.body.username }),
-  req.body.password,(err,user) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({err:err});
-      }
-      else {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
         passport.authenticate('local')(req, res, () => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
-          res.json({success:true, status: 'Registration Successful!', user: user });
-        })
-      }
-    })
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      });
+    }
+  });
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
